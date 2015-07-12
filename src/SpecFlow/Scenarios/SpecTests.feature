d708be733328c0c81c794f4ee0ab7cdd231a16e0
Rebecca Long <rebecca.long@gmail.com>
2015-07-11 17:04:40 -0700

    Initial setup for a SpecFlow + Selenium automated test setup

---
+++ src/SpecFlow/Scenarios/SpecTests.feature
@@ -0,0 +1,21 @@
+﻿Feature: SpecTests
+	
+@mytag
+Scenario: Navigate to Google
+	Given I have my browser open
+	When I type in Google's URL into the navigation bar
+	And I press Enter
+	Then the browser will navigate to Google
+
+Scenario: Perform Google Search
+	Given I have Google's homepage loaded
+	When I type "lolcat" into the search box
+	And I click "Google Search"
+	Then the search results for "lolcat" will be displayed
+
+Scenario: Click Search Result
+	Given I have performed a Google search
+	And the search results are currently displayed
+	When I click on the top search result link
+	Then I will be navigated to that website
+
---
+++ src/SpecFlow/Steps/SpecTestsSteps.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow
+{
+    [Binding]
+    public class SpecTestsSteps
+    {
+        [Given(@"I have my browser open")]
+        public void GivenIHaveMyBrowserOpen()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have Google's homepage loaded")]
+        public void GivenIHaveGoogleSHomepageLoaded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have performed a Google search")]
+        public void GivenIHavePerformedAGoogleSearch()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the search results are currently displayed")]
+        public void GivenTheSearchResultsAreCurrentlyDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I type in Google's URL into the navigation bar")]
+        public void WhenITypeInGoogleSURLIntoTheNavigationBar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I press Enter")]
+        public void WhenIPressEnter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I type ""(.*)"" into the search box")]
+        public void WhenITypeIntoTheSearchBox(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click ""(.*)""")]
+        public void WhenIClick(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on the top search result link")]
+        public void WhenIClickOnTheTopSearchResultLink()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the browser will navigate to Google")]
+        public void ThenTheBrowserWillNavigateToGoogle()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the search results for ""(.*)"" will be displayed")]
+        public void ThenTheSearchResultsForWillBeDisplayed(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I will be navigated to that website")]
+        public void ThenIWillBeNavigatedToThatWebsite()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
